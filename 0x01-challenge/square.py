7f9910d2dcba7c2301164294567e3048a742c976
samuelselasi <samelsekasi@gmail.com>
2023-07-12 21:34:10 +0000

    Test square fix

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -16,7 +16,7 @@ class Square():
         """ Area of the square """
         return (self.width * self.height)
 
-    def perimeter_of_my_square(self) -> int:
+    def PermiterOfMySquare(self) -> int:
         """Perimeter of the square"""
         return (self.width * 2) + (self.height * 2)
 
@@ -28,6 +28,6 @@ class Square():
 if __name__ == "__main__":
 
     s = Square(width=12, height=9)
-    print(f"Square dimensions: {s}")
-    print(f"Area of the square: {s.area_of_my_square()}")
-    print(f"Perimeter of the square: {s.perimeter_of_my_square()}")
+    print(s)
+    print(s.area_of_my_square())
+    print(s.PermiterOfMySquare())
