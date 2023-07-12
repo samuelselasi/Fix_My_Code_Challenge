a28cd4acc6e0db58b7eb9af763de5cc505867993
samuelselasi <samelsekasi@gmail.com>
2023-07-12 20:51:13 +0000

    Test square fix

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -11,11 +11,11 @@ class Square():
         for key, value in kwargs.items():
             setattr(self, key, value)
 
-    def AreaOfMySquare(self):
+    def area_of_my_square(self):
         """ Area of the square """
         return (self.width * self.height)
 
-    def PermiterOfMySquare(self):
+    def perimeter_of_my_square(self):
         """Perimeter of the square"""
         return ((self.width * 2) + (self.height * 2))
 
@@ -28,5 +28,5 @@ if __name__ == "__main__":
 
     s = Square(width=12, height=9)
     print(s)
-    print(s.AreaOfMySquare())
-    print(s.PermiterOfMySquare())
+    print(s.area_of_my_square())
+    print(s.perimeter_of_my_square())
