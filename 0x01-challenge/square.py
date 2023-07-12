62c89c963e88614bb49bdda3c8430c8ea3bb6408
samuelselasi <samelsekasi@gmail.com>
2023-07-12 20:42:49 +0000

    Fix square(progress)

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -13,7 +13,7 @@ class Square():
 
     def area_of_my_square(self):
         """ Area of the square """
-        return self.width * self.height
+        return (self.width * self.width)
 
     def PermiterOfMySquare(self):
         """Perimeter of the square"""
