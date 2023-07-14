153bc7367575175d93125c521c28837b6a489fa6
samuelselasi <samelsekasi@gmail.com>
2023-07-14 10:48:04 +0000

    Fix square model

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,32 +1,33 @@
 #!/usr/bin/python3
-"""Python module to calculate perimeter and area of a square"""
+""" Module for square class"""
 
 
 class Square():
-    """Class that calculates area and perimeter of a Square"""
-    def __init__(self, width: int = 0, height: int = 0, **kwargs):
-        """Function that initializes instances"""
-        self.width = width
-        self.height = height
+    """ Square class """
+    width = 0
+    height = 0
+
+    def __init__(self, *args, **kwargs):
+        """Function to inotialize instances"""
         for key, value in kwargs.items():
             setattr(self, key, value)
 
-    def area_of_my_square(self) -> int:
-        """ Area of the square """
-        return (self.width * self.height)
+    def area_of_my_square(self):
+        """Function to find area"""
+        return self.width * self.height
 
-    def perimeter_of_my_square(self) -> int:
-        """Perimeter of the square"""
+    def permiter_of_my_square(self):
+        """Function to find perimeter"""
         return (self.width * 2) + (self.height * 2)
 
-    def __str__(self) -> str:
-        """Print instances"""
+    def __str__(self):
+        """Print objects in string format"""
         return "{}/{}".format(self.width, self.height)
 
 
 if __name__ == "__main__":
-    """Creates square"""
+    """Create a square"""
     s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.perimeter_of_my_square())
+    print(s.permiter_of_my_square())
